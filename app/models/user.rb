851fb2b600f2a8c18163942640da20c46cef278a
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-12-02 07:21:13 +0300

    national id validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :national_id_number, presence: true, length: { is: 8, message: "must be 8 digits" }
+
+
+
 end
